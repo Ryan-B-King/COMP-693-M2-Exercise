1aaaaed03345c478bf9cd331eb9e5e0889b7060b
Ryan King <33941531+Ryan-B-King@users.noreply.github.com>
2023-05-17 16:21:27 -0700

    updated

--- src/employees.jsx
+++ src/employees.jsx
@@ -8,6 +8,8 @@ let chris = 'Chris Columbus'
 let amanda = 'Amanda Bynes'
 
 const element = (
+    <div>
+        <h1>Employees</h1>
         <ul style = {{'color': 'blue', 'fontSize': '24px'}}>
             <li>{ryan}</li>
             <li>{sally}</li>
@@ -18,6 +20,7 @@ const element = (
             <li>{chris}</li>
             <li>{amanda}</li>
         </ul>
-        )
+    </div>
+    )
 
 ReactDOM.render(element, document.getElementById('content'))
