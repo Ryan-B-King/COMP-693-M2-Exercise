a09de303d235350eba4160c796e4d1c7c7ed4fd9
Ryan King <33941531+Ryan-B-King@users.noreply.github.com>
2023-05-18 16:23:16 -0700

    updated exercise

--- public/employees.js
+++ public/employees.js
@@ -1,15 +1,56 @@
-let ryan = 'Ryan King';
-let sally = 'Sally Smith';
-let john = 'John Doe';
-let amol = 'Amol Shookup';
-let robin = 'Robin Banks';
-let justin = 'Justin Tyme';
-let chris = 'Chris Columbus';
-let amanda = 'Amanda Bynes';
-const element = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Employees"), /*#__PURE__*/React.createElement("ul", {
-  style: {
-    'color': 'blue',
-    'fontSize': '24px'
+// let ryan = 'Ryan King'
+// let sally = 'Sally Smith'
+// let john = 'John Doe'
+// let amol = 'Amol Shookup'
+// let robin = 'Robin Banks'
+// let justin = 'Justin Tyme'
+// let chris = 'Chris Columbus'
+// let amanda = 'Amanda Bynes'
+
+// const element = (
+
+//         <ul style = {{'color': 'blue', 'fontSize': '24px'}}>
+//             <li>{ryan}</li>
+//             <li>{sally}</li>
+//             <li>{john}</li>
+//             <li>{amol}</li>
+//             <li>{robin}</li>
+//             <li>{justin}</li>
+//             <li>{chris}</li>
+//             <li>{amanda}</li>
+//         </ul>
+
+//     )
+
+// ReactDOM.render(element, document.getElementById('content'))
+
+// // Sample function component
+// function Welcome(props) {
+//     return <h1>Hello, {props.name}</h1>
+// }
+// ReactDOM.render(<Welcome name = 'Ryan King' />, document.getElementById('content'))
+
+// Sample class component
+
+class EmployeeFilter extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement("div", null, "This is a placeholder for the Employee Filter.");
   }
-}, /*#__PURE__*/React.createElement("li", null, ryan), /*#__PURE__*/React.createElement("li", null, sally), /*#__PURE__*/React.createElement("li", null, john), /*#__PURE__*/React.createElement("li", null, amol), /*#__PURE__*/React.createElement("li", null, robin), /*#__PURE__*/React.createElement("li", null, justin), /*#__PURE__*/React.createElement("li", null, chris), /*#__PURE__*/React.createElement("li", null, amanda)));
-ReactDOM.render(element, document.getElementById('content'));
+}
+class EmployeeTable extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement("div", null, "This is a placeholder for the Employee Table.");
+  }
+}
+class EmployeeAdd extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement("div", null, "This is a placeholder for a form to add an employee.");
+  }
+}
+class EmployeeList extends React.Component {
+  render() {
+    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Employee Management Application"), /*#__PURE__*/React.createElement(EmployeeFilter, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(EmployeeTable, null), /*#__PURE__*/React.createElement("hr", null), /*#__PURE__*/React.createElement(EmployeeAdd, null));
+  }
+}
+const contentNode = document.getElementById('content');
+ReactDOM.render( /*#__PURE__*/React.createElement(EmployeeList, null), contentNode);
--- src/employees.jsx
+++ src/employees.jsx
@@ -1,26 +1,69 @@
-let ryan = 'Ryan King'
-let sally = 'Sally Smith'
-let john = 'John Doe'
-let amol = 'Amol Shookup'
-let robin = 'Robin Banks'
-let justin = 'Justin Tyme'
-let chris = 'Chris Columbus'
-let amanda = 'Amanda Bynes'
+// let ryan = 'Ryan King'
+// let sally = 'Sally Smith'
+// let john = 'John Doe'
+// let amol = 'Amol Shookup'
+// let robin = 'Robin Banks'
+// let justin = 'Justin Tyme'
+// let chris = 'Chris Columbus'
+// let amanda = 'Amanda Bynes'
 
-const element = (
-    <div>
-        <h1>Employees</h1>
-        <ul style = {{'color': 'blue', 'fontSize': '24px'}}>
-            <li>{ryan}</li>
-            <li>{sally}</li>
-            <li>{john}</li>
-            <li>{amol}</li>
-            <li>{robin}</li>
-            <li>{justin}</li>
-            <li>{chris}</li>
-            <li>{amanda}</li>
-        </ul>
-    </div>
-    )
+// const element = (
 
-ReactDOM.render(element, document.getElementById('content'))
+//         <ul style = {{'color': 'blue', 'fontSize': '24px'}}>
+//             <li>{ryan}</li>
+//             <li>{sally}</li>
+//             <li>{john}</li>
+//             <li>{amol}</li>
+//             <li>{robin}</li>
+//             <li>{justin}</li>
+//             <li>{chris}</li>
+//             <li>{amanda}</li>
+//         </ul>
+
+//     )
+
+// ReactDOM.render(element, document.getElementById('content'))
+
+
+// // Sample function component
+// function Welcome(props) {
+//     return <h1>Hello, {props.name}</h1>
+// }
+// ReactDOM.render(<Welcome name = 'Ryan King' />, document.getElementById('content'))
+
+// Sample class component
+
+class EmployeeFilter extends React.Component {
+    render() {
+        return(<div>This is a placeholder for the Employee Filter.</div>)
+    }
+}
+
+class EmployeeTable extends React.Component {
+    render() {
+        return<div>This is a placeholder for the Employee Table.</div>
+    }
+}
+
+class EmployeeAdd extends React.Component {
+    render() {
+        return<div>This is a placeholder for a form to add an employee.</div>
+    }
+}
+class EmployeeList extends React.Component {
+    render(){
+        return(
+        <React.Fragment>
+            <h1>Employee Management Application</h1>
+            <EmployeeFilter/>
+            <hr/>
+            <EmployeeTable/>
+            <hr/>
+            <EmployeeAdd/>
+        </React.Fragment>
+        )
+    }
+}
+const contentNode = document.getElementById('content');
+
+ReactDOM.render(<EmployeeList/>, contentNode)
